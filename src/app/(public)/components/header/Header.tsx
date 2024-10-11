b87b1be1b3008a77dd83653b2f9edba01f95d269
danishyameen <danishyameennew@gmail.com>
2024-10-11 07:09:54 +0500

    some changes

--- src/app/(public)/components/header/Header.tsx
+++ src/app/(public)/components/header/Header.tsx
@@ -5,8 +5,8 @@ import Link from "next/link"
 
 export default function Header(){
     return(
-        <main className="mt-5 mb-5 sticky top-0 bg-white shadow-xl w-full">
-            <header className="mt-5 grid sm:grid-cols-2 gap-2 w-full">
+        <main className="mt-5 mb-5 sticky top-0 bg-white shadow-xl w-full mx-auto">
+            <header className="mt-5 grid sm:grid-cols-2 gap-2 w-full mx-auto">
                 <div className="logo mb-2 mt-3">
                     <Image src={Logo} alt="logo" className="w-[250px] m-auto"/>
                 </div>
